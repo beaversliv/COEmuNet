1f8369397faa4c70a992d8afaa1c5a1a3698ce2c
beaversliv <shiqi.su@stfc.ac.uk>
2023-11-27 10:56:55 +0000

    add frequency loss L1

--- cnn/original/3DResNet.py
+++ cnn/original/3DResNet.py
@@ -4,10 +4,11 @@ from torch.utils.data import DataLoader, TensorDataset
 from torch.autograd import Variable
 from utils.dataloader     import CustomTransform,IntensityDataset
 from utils.model          import Net
-from focal_frequency_loss import FocalFrequencyLoss
+from utils.loss           import loss_object,mean_absolute_percentage_error, calculate_ssim_batch
 
 import h5py as h5
 import numpy as np
+from tqdm import tqdm
 import os
 import sys
 import time
@@ -58,7 +59,7 @@ def parse_args():
     return config
 config = parse_args()
 # file paths for train, vali and test
-file_statistics = '/home/dc-su2/physical_informed/cnn/unrotate_statistics.pkl'
+file_statistics = '/home/dc-su2/physical_informed/cnn/original/unrotate_statistics.pkl'
 train_file_path = [f'/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/train_{i}.hdf5' for i in range(4)]
 vali_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/vali.hdf5']
 test_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/test.hdf5']
@@ -77,37 +78,27 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ### set a model ###
 model = Net()
-model.to(device)   
-
-def loss_object(target,pred):
-    # discrete fourier transform
-    target_freq = torch.fft.fft2(target)
-    pred_freq = torch.fft.fft2(pred)
-    # MAE in frequency domain
-    loss_freq = torch.mean(torch.abs(target_freq-pred_freq))
-    # MSE in spatial domain
-    loss_mse  = nn.functional.mse_loss(pred,target)
-
-    freq_loss_scale = 1.0
-    mse_loss_scale = 1.0
-    loss = mse_loss_scale*loss_mse +freq_loss_scale * loss_freq
-    return loss
+# model = nn.DataParallel(model,device_ids=[0,1,2])
+model.to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], weight_decay=1e-2, betas=(0.9, 0.999))
 
 ### train step ###
 def train(epoch):
-    epoch_loss = 0.
+    total_loss = 0.
     model.train() 
     for bidx, samples in enumerate(train_dataloader):
         data, target = Variable(samples[0]).to(device), Variable(samples[1]).to(device)
 
         optimizer.zero_grad()
         latent,output = model(data)
-        loss = loss_object(target, output)
+        loss = loss_object(target,output,use_freq_loss=True, use_perceptual_loss=False)
+
         loss.backward()
         optimizer.step()
-        epoch_loss += loss.detach().cpu().numpy()
+        total_loss += loss.detach().cpu().numpy()
+
+    epoch_loss = total_loss/len(train_dataloader) # divide number of batches
     print('Train Epoch: {}/{} Loss: {:.4f}'.format(
             epoch, config['epochs'], epoch_loss))
     return epoch_loss
@@ -121,7 +112,7 @@ def test(epoch,dataloader):
     for bidx, samples in enumerate(dataloader):
         data, target = Variable(samples[0]).to(device), Variable(samples[1]).to(device)
         latent,pred = model(data)
-        loss = loss_object(target, pred)
+        loss = loss_object(target, pred,use_freq_loss=True, use_perceptual_loss=False)
         
         P.append(pred.detach().cpu().numpy())
         T.append(target.detach().cpu().numpy())
@@ -136,7 +127,7 @@ def test(epoch,dataloader):
 def run():
     losses = []
     vl     = []
-    for epoch in range(config['epochs']):
+    for epoch in tqdm(range(config['epochs'])):
         epoch_loss = train(epoch)
         _,_,val_loss   = test(epoch,vali_dataloader)
         losses.append(epoch_loss)
@@ -147,14 +138,20 @@ def main():
     start = time.time()
     losses,vl = run()
     end = time.time()
-    print(f'running time:{(end-start)/60} mins')
+    print(f'running time:{(end-start)/60} mins\n')
     pred, target, _ = test(config['epochs'],test_dataloader)
+
+    ### other quantitive measure ###
+    mean_error, median_error = mean_absolute_percentage_error(target,pred)
+    print('mean relative error: {:.4f}\n, median relative error: {:.4f}'.format(mean_error,median_error))
+    avg_ssim = calculate_ssim_batch(target,pred)
+    print('SSIM: {:.4f}'.format(avg_ssim))
 
     data = (losses, vl, pred, target)
     # Save to a pickle file
-    with open("/home/dc-su2/physical_informed/cnn/results/history.pkl", "wb") as pickle_file:
+    with open("/home/dc-su2/physical_informed/cnn/original/results/history.pkl", "wb") as pickle_file:
         pickle.dump(data, pickle_file)
-    torch.save(model,'/home/dc-su2/physical_informed/cnn/results/model.pth')
+    torch.save(model,'/home/dc-su2/physical_informed/cnn/original/results/model.pth')
 
     for i in range(0,600,50):
         fig, axs = plt.subplots(1, 2,figsize=(12, 5))
@@ -165,7 +162,7 @@ def main():
         im2 = axs[1].imshow(pred[i][0],vmin=np.min(target[i][0]),vmax = np.max(target[i][0]))
         axs[1].set_title('prediction')
         fig.colorbar(im2,ax=axs[1])
-        plt.savefig('/home/dc-su2/physical_informed/cnn/results/img/ex{}.png'.format(i))
+        plt.savefig('/home/dc-su2/physical_informed/cnn/original/results/img/ex{}.png'.format(i))
         plt.close()
 
 if __name__ == '__main__':
