c2ba453826fc48bae1799ceb767d8f95d5824700
beaversliv <shiqi.su@stfc.ac.uk>
2024-02-28 19:56:09 +0000

    removed outliers and re-run the experiments with face-on data

--- cnn/3DResNet.py
+++ cnn/3DResNet.py
@@ -2,16 +2,20 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 from torch.autograd import Variable
+# custom helper functions
 from utils.dataloader     import CustomTransform,IntensityDataset
 from utils.model          import Net
-from utils.loss           import loss_object,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.loss           import Lossfunction,ResNetFeatures,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.plot           import img_plt,history_plt
 
+# helper packages
 import h5py as h5
 import numpy as np
-from tqdm import tqdm
 import os
 import sys
 import time
+import logging
+from tqdm                 import tqdm
 import pickle
 import argparse
 from collections import OrderedDict
@@ -30,17 +34,20 @@ else:
 
 
 # Global Constants
-np.random.seed(1234)
-torch.manual_seed(1234) 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+seed = 1234
+np.random.seed(seed)
+torch.manual_seed(seed) 
+torch.cuda.manual_seed(seed)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_dir', type = str, default = os.getcwd())
     parser.add_argument('--model_name', type = str, default = '3dResNet')
     parser.add_argument('--dataset', type = str, default = 'p3droslo')
-    parser.add_argument('--epochs', type = int, default = 200)
+    parser.add_argument('--epochs', type = int, default = 100)
     parser.add_argument('--batch_size', type = int, default = 32)
-    parser.add_argument('--lr', type = float, default = 1e-4)
+    parser.add_argument('--lr', type = float, default = 1e-3)
     parser.add_argument('--lr_decay', type = float, default = 0.95)
 
 
@@ -58,113 +65,125 @@ def parse_args():
             ])
     
     return config
-config = parse_args()
-# file paths for train, vali and test
-file_statistics = '/home/dc-su2/physical_informed/cnn/original/unrotate_statistics.pkl'
-train_file_path = [f'/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/train_{i}.hdf5' for i in range(4)]
-vali_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/vali.hdf5']
-test_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/Batches/test.hdf5']
-
-custom_transform = CustomTransform(file_statistics)
-train_dataset= IntensityDataset(train_file_path,transform=custom_transform)
-train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True,num_workers=2)
-
-vali_dataset= IntensityDataset(vali_file_path,transform=custom_transform)
-vali_dataloader = DataLoader(vali_dataset, batch_size=config['batch_size'], shuffle=True,num_workers=2)
-
-test_dataset= IntensityDataset(test_file_path,transform=custom_transform)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-### set a model ###
-model = Net()
-model = nn.DataParallel(model,device_ids=[0,1])
-model.to(device)
-
-optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], weight_decay=1e-2, betas=(0.9, 0.999))
 
 ### train step ###
-def train(epoch):
-    total_loss = 0.
-    model.train() 
-    for bidx, samples in enumerate(train_dataloader):
-        data, target = Variable(samples[0]).to(device), Variable(samples[1]).to(device)
-
-        optimizer.zero_grad()
-        latent,output = model(data)
-        loss = loss_object(target,output,use_freq_loss=True, use_perceptual_loss=False)
-
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.detach().cpu().numpy()
-
-    epoch_loss = total_loss/len(train_dataloader) # divide number of batches
-    print('Train Epoch: {}/{} Loss: {:.4f}'.format(
-            epoch, config['epochs'], epoch_loss))
-    return epoch_loss
-
-### test/val step ###
-def test(epoch,dataloader):
-    model.eval()
-    P = []
-    T = []
-    L = []
-    for bidx, samples in enumerate(dataloader):
-        data, target = Variable(samples[0]).to(device), Variable(samples[1]).to(device)
-        latent,pred = model(data)
-        loss = loss_object(target, pred,use_freq_loss=True, use_perceptual_loss=False)
-        
-        P.append(pred.detach().cpu().numpy())
-        T.append(target.detach().cpu().numpy())
-        L.append(loss.detach().cpu().numpy())
+class Trainer:
+    def __init__(self,model,loss_object,optimizer,train_dataloader,test_dataloader,config,device):
+        self.model = model
+        self.loss_object = loss_object
+        self.optimizer   = optimizer
+        self.train_dataloader = train_dataloader
+        self.test_dataloader  = test_dataloader
+        self.config = config
+        self.device = device
     
-    print('Test Epoch: {}/{} Loss: {:.4f}\n'.format(
-            epoch, config['epochs'], np.mean(L)))
-    P = np.vstack(P)
-    T = np.vstack(T)
-    return P, T, L
-### run ###
-def run():
-    losses = []
-    vl     = []
-    for epoch in tqdm(range(config['epochs'])):
-        epoch_loss = train(epoch)
-        _,_,val_loss   = test(epoch,vali_dataloader)
-        losses.append(epoch_loss)
-        vl.append(np.mean(val_loss))
-    return losses,vl
+    def train(self):
+        total_loss = 0.
+        self.model.train()
+         
+        for bidx, samples in enumerate(self.train_dataloader):
+            data, target = Variable(samples[0]).to(self.device), Variable(samples[1]).to(self.device)
+            self.optimizer.zero_grad()
+            latent,output = self.model(data)
+            loss = self.loss_object(target, output)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.detach().cpu().numpy()
+        epoch_loss = total_loss / len(self.train_dataloader)  # divide number of batches
+
+        return epoch_loss
+
+    
+    def test(self):
+        self.model.eval()
+        P = []
+        T = []
+        L = []
+        for bidx, samples in enumerate(self.test_dataloader):
+            data, target = Variable(samples[0]).to(self.device), Variable(samples[1]).to(self.device)
+            latent,pred = self.model(data)
+            loss = self.loss_object(target, pred)
+            
+            P.append(pred.detach().cpu().numpy())
+            T.append(target.detach().cpu().numpy())
+            L.append(loss.detach().cpu().numpy())
+        P = np.vstack(P)
+        T = np.vstack(T)
+        return P,T,np.mean(L)
+    def run(self):
+        tr_losses = []
+        vl_losses = []
+        for epoch in tqdm(range(self.config['epochs'])):
+            epoch_loss = self.train()
+            torch.cuda.empty_cache()  # Clear cache after training
+            
+            _, _, val_loss = self.test()
+            torch.cuda.empty_cache()  # Clear cache after evaluation
+            tr_losses.append(epoch_loss)
+            vl_losses.append(val_loss)
+            print('Train Epoch: {}/{} Loss: {:.4f}'.format(
+                    epoch, self.config['epochs'], epoch_loss))
+            print('Test Epoch: {}/{} Loss: {:.4f}\n'.format(
+                epoch, self.config["epochs"], val_loss))
+            
+        return tr_losses, vl_losses
 
 def main():
-    start = time.time()
-    losses,vl = run()
-    end = time.time()
-    print(f'running time:{(end-start)/60} mins\n')
-    pred, target, _ = test(config['epochs'],test_dataloader)
+    config = parse_args()
+    # file paths for train, vali and test
+    file_statistics = '/home/dc-su2/physical_informed/cnn/original/unrotate_statistics.pkl'
+    train_file_path = [f'/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/face_on/clean_train_{i}.hdf5' for i in range(4)]
+    vali_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/faceon/clean_vali.hdf5']
+    test_file_path  = ['/home/dc-su2/rds/rds-dirac-dp147/vtu_oldmodels/Magritte-examples/physical_forward/cnn/faceon/clean_test.hdf5']
 
-    ### other quantitive measure ###
+    custom_transform = CustomTransform(file_statistics)
+    train_dataset= IntensityDataset(train_file_path,transform=custom_transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True,num_workers=2)
+
+    vali_dataset= IntensityDataset(vali_file_path,transform=custom_transform)
+    vali_dataloader = DataLoader(vali_dataset, batch_size=config['batch_size'], shuffle=True,num_workers=2)
+
+    test_dataset= IntensityDataset(test_file_path,transform=custom_transform)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    ### set a model ###
+    model = Net().to(device)
+   
+    resnet34 = ResNetFeatures().to(device)
+    loss_object = Lossfunction(resnet34,mse_loss_scale = 0.5,freq_loss_scale=0.5, perceptual_loss_scale=0.0)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], betas=(0.9, 0.999))
+
+
+    ### start training ###
+    start = time.time()
+    # Assuming model, loss_object, optimizer, train_dataloader, test_dataloader, config, and device are defined
+    trainer = Trainer(model, loss_object, optimizer, train_dataloader, test_dataloader, config, device)
+    tr_losses, vl_losses = trainer.run()
+    end = time.time()
+    print(f'running time:{(end-start)/60} mins')
+    
+    ### validation ###
+    pred, target, test_loss = trainer.test()
+    print('Test Epoch: {} Loss: {:.4f}\n'.format(
+                config["epochs"], test_loss))
+    data = (tr_losses, vl_losses,pred, target)
+    
+    # with open("/home/dc-su2/physical_informed/cnn/original/results/history.pkl", "wb") as pickle_file:
+    #     pickle.dump(data, pickle_file)
+    
+
     mean_error, median_error = mean_absolute_percentage_error(target,pred)
     print('mean relative error: {:.4f}\n, median relative error: {:.4f}'.format(mean_error,median_error))
     avg_ssim = calculate_ssim_batch(target,pred)
     print('SSIM: {:.4f}'.format(avg_ssim))
+    # plot pred-targ
+    img_plt(target,pred,path='/home/dc-su2/physical_informed/cnn/rotate/results/img/')
+    history_plt(tr_losses,vl_losses,path='/home/dc-su2/physical_informed/cnn/original/results/')
+    # torch.save(model.state_dict(),'/home/dc-su2/physical_informed/cnn/original/results/model.pth')
 
-    data = (losses, vl, pred, target)
-    # Save to a pickle file
-    # with open("/home/dc-su2/physical_informed/cnn/original/results/history.pkl", "wb") as pickle_file:
-    #     pickle.dump(data, pickle_file)
-    # torch.save(model,'/home/dc-su2/physical_informed/cnn/original/results/model.pth')
-
-    # for i in range(0,600,50):
-    #     fig, axs = plt.subplots(1, 2,figsize=(12, 5))
-    #     im1 = axs[0].imshow(target[i][0],vmin=np.min(target[i][0]),vmax = np.max(target[i][0]))
-    #     axs[0].set_title('target')
-    #     fig.colorbar(im1,ax=axs[0])
-
-    #     im2 = axs[1].imshow(pred[i][0],vmin=np.min(target[i][0]),vmax = np.max(target[i][0]))
-    #     axs[1].set_title('prediction')
-    #     fig.colorbar(im2,ax=axs[1])
-    #     plt.savefig('/home/dc-su2/physical_informed/cnn/original/results/img/ex{}.png'.format(i))
-        # plt.close()
 
 if __name__ == '__main__':
     main()
