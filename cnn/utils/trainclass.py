8028e00539db54a791f1fbb57602787581bc0004
beaversliv <shiqi.su@stfc.ac.uk>
2024-03-12 11:10:08 +0000

    extract Train class

---
+++ cnn/utils/trainclass.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+from torch.autograd import Variable
+from tqdm                 import tqdm
+import numpy as np
+
+class Trainer:
+    def __init__(self,model,loss_object,optimizer,train_dataloader,test_dataloader,config,device):
+        self.model = model
+        self.loss_object = loss_object
+        self.optimizer   = optimizer
+        self.train_dataloader = train_dataloader
+        self.test_dataloader  = test_dataloader
+        self.config = config
+        self.device = device
+    
+    def train(self):
+        total_loss = 0.
+        self.model.train()
+         
+        for bidx, samples in enumerate(self.train_dataloader):
+            data, target = Variable(samples[0]).to(self.device), Variable(samples[1]).to(self.device)
+            self.optimizer.zero_grad()
+            latent,output = self.model(data)
+            loss = self.loss_object(target, output)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.detach().cpu().numpy()
+        epoch_loss = total_loss / len(self.train_dataloader)  # divide number of batches
+
+        return epoch_loss
+
+    
+    def test(self):
+        self.model.eval()
+        P = []
+        T = []
+        L = []
+        for bidx, samples in enumerate(self.test_dataloader):
+            data, target = Variable(samples[0]).to(self.device), Variable(samples[1]).to(self.device)
+            latent,pred = self.model(data)
+            loss = self.loss_object(target, pred)
+            
+            P.append(pred.detach().cpu().numpy())
+            T.append(target.detach().cpu().numpy())
+            L.append(loss.detach().cpu().numpy())
+        P = np.vstack(P)
+        T = np.vstack(T)
+        return P,T,np.mean(L)
+    def run(self):
+        tr_losses = []
+        vl_losses = []
+        for epoch in tqdm(range(self.config['epochs'])):
+            epoch_loss = self.train()
+            torch.cuda.empty_cache()  # Clear cache after training
+            
+            _, _, val_loss = self.test()
+            torch.cuda.empty_cache()  # Clear cache after evaluation
+            tr_losses.append(epoch_loss)
+            vl_losses.append(val_loss)
+            print('Train Epoch: {}/{} Loss: {:.4f}'.format(
+                    epoch, self.config['epochs'], epoch_loss))
+            print('Test Epoch: {}/{} Loss: {:.4f}\n'.format(
+                epoch, self.config["epochs"], val_loss))
+            
+        return tr_losses, vl_losses
