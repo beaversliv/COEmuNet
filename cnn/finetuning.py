8e58b47643fe37d9dde501d26fc09bb52c9d3448
beaversliv <shiqi.su@stfc.ac.uk>
2024-05-31 13:20:38 +0100

    load the best model and train longer

---
+++ cnn/finetuning.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn       as nn
+from torch.utils.data import DataLoader, TensorDataset
+from torch.autograd   import Variable
+
+from utils.ResNet3DModel  import Net
+from utils.dataloader     import CustomTransform,IntensityDataset
+from utils.loss           import SobelMse
+from utils.preprocessing  import preProcessing
+from utils.trainclass     import ddpTrainer
+
+import time
+import numpy              as np
+import h5py               as h5
+import logging
+from utils.ResNet3DModel  import Net3D,Net
+from thop                   import profile
+from sklearn.model_selection      import train_test_split 
+import pickle
+
+# Global Constants
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+seed = 1234
+np.random.seed(seed)
+torch.manual_seed(seed) 
+torch.cuda.manual_seed(seed)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_dir', type = str, default = os.getcwd())
+    parser.add_argument('--model_name', type = str, default = '3dResNet')
+    parser.add_argument('--dataset', type = str, default = 'p3droslo')
+    parser.add_argument('--epochs', type = int, default = 100)
+    parser.add_argument('--batch_size', type = int, default = 128)
+    parser.add_argument('--lr', type = float, default = 1e-3)
+    parser.add_argument('--lr_decay', type = float, default = 1e-4)
+
+
+    args = parser.parse_args()
+    
+    config = OrderedDict([
+            ('path_dir', args.path_dir),
+            ('model_name', args.model_name),
+            
+            ('dataset', args.dataset),
+            ('epochs', args.epochs),
+            ('batch_size', args.batch_size),
+            ('lr', args.lr),
+            ('lr_decay', args.lr_decay)
+            ])
+    
+    return config
+def postProcessing(y):
+    
+    min_ = -47.387955
+    median = 8.168968
+    y = y*median + min_
+    y = np.exp(y)
+    return y
+
+def main():
+    config = parse_args()
+    data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
+    x,y = data_gen.get_data()
+    
+    # train test split
+    xtr, xte, ytr,yte = train_test_split(x,y,test_size=0.2,random_state=42)
+    # xtr,xte = x[:800],x[800:1000]
+    # ytr,yte = y[:800],y[800:1000]
+    xtr = torch.tensor(xtr,dtype=torch.float32)
+    ytr = torch.tensor(ytr,dtype=torch.float32)
+    xte = torch.tensor(xte,dtype=torch.float32)
+    yte = torch.tensor(yte,dtype=torch.float32)
+
+    train_dataset = TensorDataset(xtr, ytr)
+    test_dataset = TensorDataset(xte, yte)
+
+    ## torch data loader ###
+    train_dataloader = DataLoader(train_dataset, batch_size= config['batch_size'], shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    ### set a model ###
+    model = Net().to(device)
+    model_dic = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/model.pth'
+    model.load_state_dict(torch.load(model_dic,map_location=device))
+    
+    loss_object = SobelMse(device,0.8,0.2)
+    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], betas=(0.9, 0.999),weight_decay=config['lr_decay'])
+
+
+    ### start training ###
+    start = time.time()
+    # Assuming model, loss_object, optimizer, train_dataloader, test_dataloader, config, and device are defined
+    trainer = ddpTrainer(model, loss_object, optimizer, train_dataloader, test_dataloader, config, device)
+    tr_losses, vl_losses = trainer.run()
+    end = time.time()
+    print(f'running time:{(end-start)/60} mins')
+    
+    ### validation ###
+    pred, target, test_loss = trainer.test()
+    print('Test Epoch: {} Loss: {:.4f}\n'.format(
+                config["epochs"], test_loss))
+
+    ### save history and preds ###            
+    pickle_file_path = "/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/history.pkl"
+    try:
+        with open(pickle_file_path, "wb") as pickle_file:
+            pickle.dump({
+                'history': {'train_loss': tr_losses, 'val_loss': vl_losses},
+                'targets': target,
+                'predictions': pred
+            }, pickle_file)
+        print(f"Data successfully saved to {pickle_file_path}")
+    except Exception as e:
+        print(f"Error saving data to pickle file: {e}")
+
+    original_target = postProcessing(target)
+    original_pred = postProcessing(pred)
+    print(f'relative loss {MaxRel(original_target,original_pred):.5f}%')
+
+    avg_ssim = calculate_ssim_batch(target,pred)
+    for freq in range(len(avg_ssim)):
+        print(f'frequency {freq + 1} has ssim {avg_ssim[freq]:.4f}')
+    torch.save(model.state_dict(),'/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/model.pth')
+
+
+if __name__ == '__main__':
+    main()
