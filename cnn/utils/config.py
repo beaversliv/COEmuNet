ddd99f85d7a7a8bbc25b20950c7e3c6e16c0deda
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-06 12:41:21 +0100

    write config in a function and use in all training script

--- cnn/utils/config.py
+++ cnn/utils/config.py
@@ -1,6 +1,44 @@
 import argparse
 import os
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_dir', type = str, default = os.getcwd())
+    parser.add_argument('--seed',type = int, default = 1223)
+    parser.add_argument('--dataset', type = str, default = 'p3droslo')
+    parser.add_argument('--model_grid',type=int,default= 64,help='grid of hydro model:[32,64,128]')
+    parser.add_argument('--save_path',type =str, default = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/')
+    parser.add_argument('--logfile',type = str, default = 'log_file.txt')
+    parser.add_argument('--model_name', type = str, default = 'model.pth')
+    parser.add_argument('--history', type = str, default = 'history.pkl')
+    parser.add_argument('--epochs', type = int, default = 1)
+    parser.add_argument('--batch_size', type = int, default = 128)
+    parser.add_argument('--lr', type = float, default = 1e-3)
+    parser.add_argument('--lr_decay', type = float, default = 0.95)
+    parser.add_argument('--alpha', type = float, default = 0.8)
+    parser.add_argument('--beta', type = float, default = 0.2)
+
+
+    args = parser.parse_args()    
+    config = OrderedDict([
+            ('path_dir', args.path_dir),
+            ('seed', args.seed),
+            ('dataset', args.dataset),
+            ('model_grid', args.model_grid),
+            ('save_path',args.save_path),
+            ('logfile',args.logfile),
+            ('model_name', args.model_name),
+            ('history', args.history),
+            ('epochs', args.epochs),
+            ('batch_size', args.batch_size),
+            ('lr', args.lr),
+            ('lr_decay', args.lr_decay),
+            ('alpha', args.alpha),
+            ('beta', args.beta)
+            ])
+    
+    return config
+
 def faceon_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_dir', type = str, default = os.getcwd())
