f255a289d3402bef39da8c2cd025298948aef769
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-06 20:46:19 +0100

    Fine tune with ddp

--- cnn/ddp.py
+++ cnn/ddp.py
@@ -7,9 +7,10 @@ from torch.nn.parallel        import DistributedDataParallel as DDP
 from torch.profiler           import profile, record_function, ProfilerActivity
 from torch.optim.lr_scheduler import StepLR
 from utils.preprocessing  import preProcessing
-from utils.ResNet3DModel  import Net
-from utils.loss           import SobelMse,FreqMae,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.ResNet3DModel  import FinetuneNet
+from utils.loss           import SobelMse,FreqMae,SobelMae,mean_absolute_percentage_error, calculate_ssim_batch
 from utils.trainclass     import ddpTrainer
+from utils.config         import parse_args
 # from utils.plot           import img_plt
 
 import h5py as h5
@@ -25,11 +26,6 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 from sklearn.model_selection      import train_test_split 
 import socket                     
-seed = 1234
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-
 def setup(rank, world_size):
     "Sets up the process group and configuration for PyTorch Distributed Data Parallelism"
 
@@ -57,36 +53,6 @@ def setup(rank, world_size):
 def cleanup():
     "Cleans up the distributed environment"
     dist.destroy_process_group()
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path_dir', type = str, default = os.getcwd())
-    parser.add_argument('--model_name', type = str, default = '3dResNet')
-    parser.add_argument('--dataset', type = str, default = 'p3droslo')
-    parser.add_argument('--model_grid',type=int,default= 64,help='grid of hydro model:[32,64,128]')
-    parser.add_argument('--save_path',type =str, default = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/')
-    parser.add_argument('--logfile',type = str, default = 'ddp_log_file')
-    parser.add_argument('--epochs', type = int, default = 1000)
-    parser.add_argument('--batch_size', type = int, default = 128)
-    parser.add_argument('--lr', type = float, default = 1e-3)
-    parser.add_argument('--lr_decay', type = float, default = 1e-4)
-
-
-    args = parser.parse_args()
-    
-    config = OrderedDict([
-            ('path_dir', args.path_dir),
-            ('model_name', args.model_name),
-            ('dataset', args.dataset),
-            ('model_grid', args.model_grid),
-            ('save_path',args.save_path),
-            ('logfile',args.logfile),
-            ('epochs', args.epochs),
-            ('batch_size', args.batch_size),
-            ('lr', args.lr),
-            ('lr_decay', args.lr_decay)
-            ])
-    
-    return config
 def main(): 
     # Initialize any necessary components for DDP
     world_size    = int(os.environ.get("SLURM_NTASKS"))
@@ -95,8 +61,13 @@ def main():
     num_workers = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
     print(f"Hello from rank {rank} of {world_size} on {socket.gethostname()} where there are" \
           f" {gpus_per_node} allocated GPUs per node.", flush=True)
-    setup(rank, world_size) 
+    setup(rank, world_size)
+
     config = parse_args()
+    np.random.seed(config['seed'])
+    torch.manual_seed(config['seed'])
+    torch.cuda.manual_seed_all(config['seed'])
+
     data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
     x,y = data_gen.get_data()
     # train test split
@@ -115,27 +86,34 @@ def main():
 
     local_rank = rank - gpus_per_node * (rank // gpus_per_node)
     torch.cuda.set_device(local_rank)
-    model = Net(config['model_grid'])
+
+    model = FinetuneNet(config['model_grid'])
+    model_dic = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/best_model.pth'
+    checkpoint = torch.load(model_dic,map_location=torch.device('cpu'))
+    model.encoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('encoder')}
+    model.decoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('decoder')}
+
+    # model = Net(config['model_grid'])
     model = model.to(local_rank)
     ddp_model = DDP(model, device_ids=[local_rank],find_unused_parameters=True)
 
     # Define the optimizer for the DDP model
     optimizer = torch.optim.Adam(ddp_model.parameters(), lr=config['lr'], betas=(0.9, 0.999))
-    loss_object = SobelMse(local_rank,alpha=0.8,beta=0.2)
+    cheduler = StepLR(optimizer, step_size=100, gamma=0.1)
+    loss_object = SobelMse(local_rank, alpha=0.8,beta=0.2)
     # Create the Trainer instance
-    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size)
+    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size,scheduler=None)
     
     # Run the training and testing
-     ### start training ###
+    ### start training ###
+    trainer.eval()
+
     start = time.time()
-    print(f'rank {rank} starts training')
-    history = trainer.run()
+    print(f'rank {rank} starts training\n')
+    trainer.run()
     end = time.time()
     print(f'running time:{(end-start)/60} mins')
-    trainer.save(model_path = config['save_path'] + 'ddp_model.pth', 
-                 history_path = config['save_path'] + 'ddp_history.pkl',
-                 history = history,
-                 world_size = world_size)
+    trainer.save(False)
    
     # Clean up
     cleanup()
