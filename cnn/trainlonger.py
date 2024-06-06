ee6055c439741521c3ac760048904413d9efe65d
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-06 13:13:52 +0100

    add lr schedular

--- cnn/trainlonger.py
+++ cnn/trainlonger.py
@@ -9,7 +9,8 @@ from torch.optim.lr_scheduler import StepLR
 from utils.preprocessing  import preProcessing
 from utils.trainclass     import ddpTrainer
 from utils.ResNet3DModel  import Net
-from utils.loss           import SobelMse,FreqMae,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.loss           import SobelMse,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.config         import parse_args
 # from utils.plot           import img_plt
 
 import h5py as h5
@@ -30,6 +31,7 @@ seed = 1234
 np.random.seed(seed)
 torch.manual_seed(seed) 
 torch.cuda.manual_seed(seed)
+# torch.backends.cudnn.deterministic = True
 def setup(rank, world_size):
     "Sets up the process group and configuration for PyTorch Distributed Data Parallelism"
 
@@ -57,38 +59,7 @@ def setup(rank, world_size):
 def cleanup():
     "Cleans up the distributed environment"
     dist.destroy_process_group()
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path_dir', type = str, default = os.getcwd())
-    parser.add_argument('--dataset', type = str, default = 'p3droslo')
-    parser.add_argument('--model_grid',type=int,default= 64,help='grid of hydro model:[32,64,128]')
-    parser.add_argument('--save_path',type =str, default = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/')
-    parser.add_argument('--logfile',type = str, default = '4GPUtrainlonger_log_file')
-    parser.add_argument('--model_name', type = str, default = '4GPUtrainlonger.pth')
-    parser.add_argument('--patience',type = int, default = 100, help='early stop patience')
-    parser.add_argument('--epochs', type = int, default = 5000)
-    parser.add_argument('--batch_size', type = int, default = 32)
-    parser.add_argument('--lr', type = float, default = 4*1e-3)
-    parser.add_argument('--lr_decay', type = float, default = 0.95)
-
-
-    args = parser.parse_args()
-    
-    config = OrderedDict([
-            ('path_dir', args.path_dir),
-            ('dataset', args.dataset),
-            ('model_grid', args.model_grid),
-            ('save_path',args.save_path),
-            ('logfile',args.logfile),
-            ('model_name',args.model_name),
-            ('patience',args.patience),
-            ('epochs', args.epochs),
-            ('batch_size', args.batch_size),
-            ('lr', args.lr),
-            ('lr_decay', args.lr_decay)
-            ])
-    
-    return config
+   
 def main(): 
     # Initialize any necessary components for DDP
     world_size    = int(os.environ.get("SLURM_NTASKS"))
@@ -99,11 +70,16 @@ def main():
     print(f"Hello from rank {rank} of {world_size} on {socket.gethostname()} where there are" \
           f" {gpus_per_node} allocated GPUs per node.", flush=True)
     setup(rank, world_size) 
+
     config = parse_args()
-    data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
-    x,y = data_gen.get_data()
+    np.random.seed(config['seed'])
+    torch.manual_seed(config['seed'])
+    torch.cuda.manual_seed_all(config['seed'])
+
+    # data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
+    # x,y = data_gen.get_data()
     # train test split
-    # x,y = np.random.rand(100,3,64,64,64),np.random.rand(100,1,64,64)
+    x,y = np.random.rand(64,3,64,64,64),np.random.rand(64,1,64,64)
     xtr, xte, ytr,yte = train_test_split(x,y,test_size=0.2,random_state=42)
     xtr = torch.tensor(xtr,dtype=torch.float32)
     ytr = torch.tensor(ytr,dtype=torch.float32)
@@ -129,19 +105,21 @@ def main():
 
     # Define the optimizer for the DDP model
     optimizer = torch.optim.Adam(ddp_model.parameters(), lr=config['lr'], betas=(0.9, 0.999))
+    scheduler = StepLR(optimizer, step_size=100, gamma=0.1)
     loss_object = SobelMse(local_rank, alpha=0.8,beta=0.2)
     # Create the Trainer instance
-    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size)
+    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size,scheduler=None)
     
     # Run the training and testing
     ### start training ###
+    trainer.eval()
+
     start = time.time()
-    print(f'rank {rank} starts training')
+    print(f'rank {rank} starts training\n')
     trainer.run()
     end = time.time()
     print(f'running time:{(end-start)/60} mins')
-    trainer.save(history_path = config['save_path'] + '4GPUtrainLonger_history.pkl',
-                 world_size = world_size)
+    trainer.save(False)
    
     # Clean up
     cleanup()
