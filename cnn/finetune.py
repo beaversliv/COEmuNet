983340b94d204764cdb62b96386e5b30b327885d
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-11 14:03:39 +0100

    update

--- cnn/finetune.py
+++ cnn/finetune.py
@@ -1,78 +1,76 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
-from torch.autograd import Variable
-# custom helper functions
-from utils.dataloader     import CustomTransform,IntensityDataset
-from utils.ResNet3DModel          import Net,FinetuneNet
-from utils.loss           import SobelMse,MaxRel, calculate_ssim_batch
-from utils.plot           import img_plt,history_plt
+import torch.multiprocessing  as mp
+import torch.distributed      as dist
+from torch.autograd           import Variable
+from torch.utils.data         import DataLoader, TensorDataset,DistributedSampler
+from torch.nn.parallel        import DistributedDataParallel as DDP
+from torch.profiler           import profile, record_function, ProfilerActivity
+from torch.optim.lr_scheduler import StepLR
 from utils.preprocessing  import preProcessing
-from utils.trainclass     import Trainer
-# helper packages
+from utils.ResNet3DModel  import FinetuneNet
+from utils.loss           import SobelMse,FreqMae,SobelMae,mean_absolute_percentage_error, calculate_ssim_batch
+from utils.trainclass     import ddpTrainer
+from utils.config         import parse_args
+# from utils.plot           import img_plt
+
 import h5py as h5
 import numpy as np
+from tqdm import tqdm
 import os
 import sys
+import json
 import time
-import logging
-from tqdm                 import tqdm
 import pickle
 import argparse
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 from sklearn.model_selection      import train_test_split 
+import socket                     
+def setup(rank, world_size):
+    "Sets up the process group and configuration for PyTorch Distributed Data Parallelism"
 
-# Global Constants
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-seed = 1234
-np.random.seed(seed)
-torch.manual_seed(seed) 
-torch.cuda.manual_seed(seed)
+    def get_free_port():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('', 0))
+            return s.getsockname()[1]
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path_dir', type = str, default = os.getcwd())
-    parser.add_argument('--model_name', type = str, default = '3dResNet')
-    parser.add_argument('--dataset', type = str, default = 'p3droslo')
-    parser.add_argument('--model_grid',type=int,default= 64,help='grid of hydro model:[32,64,128]')
-    parser.add_argument('--save_path',type =str, default = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/')
-    parser.add_argument('--logfile',type = str, default = 'fineTune_log_file')
-    parser.add_argument('--epochs', type = int, default = 100)
-    parser.add_argument('--batch_size', type = int, default = 128)
-    parser.add_argument('--lr', type = float, default = 1e-3)
-    parser.add_argument('--lr_decay', type = float, default = 0.95)
-
-
-    args = parser.parse_args()
+    # Ensure MASTER_ADDR is set
+    if "MASTER_ADDR" not in os.environ:
+        raise ValueError("MASTER_ADDR environment variable is not set")
     
-    config = OrderedDict([
-            ('path_dir', args.path_dir),
-            ('model_name', args.model_name),
-            ('dataset', args.dataset),
-            ('model_grid', args.model_grid),
-            ('save_path',args.save_path),
-            ('logfile',args.logfile),
-            ('epochs', args.epochs),
-            ('batch_size', args.batch_size),
-            ('lr', args.lr),
-            ('lr_decay', args.lr_decay)
-            ])
+    # Set MASTER_PORT to a free port
+    if "MASTER_PORT" not in os.environ:
+        free_port = get_free_port()
+        os.environ["MASTER_PORT"] = str(free_port)
     
-    return config
-def postProcessing(y):
-    
-    min_ = -47.387955
-    median = 8.168968
-    y = y*median + min_
-    y = np.exp(y)
-    return y
-def main():
+    # Ensure MASTER_PORT is set
+    if "MASTER_PORT" not in os.environ or os.environ["MASTER_PORT"] is None:
+        raise ValueError("MASTER_PORT environment variable is not set")
+
+    # Initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+def cleanup():
+    "Cleans up the distributed environment"
+    dist.destroy_process_group()
+def main(): 
+    # Initialize any necessary components for DDP
+    world_size    = int(os.environ.get("SLURM_NTASKS"))
+    rank          = int(os.environ.get("SLURM_PROCID"))
+    gpus_per_node = int(os.environ.get("SLURM_GPUS_ON_NODE"))
+    num_workers = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
+    print(f"Hello from rank {rank} of {world_size} on {socket.gethostname()} where there are" \
+          f" {gpus_per_node} allocated GPUs per node.", flush=True)
+    setup(rank, world_size)
+
     config = parse_args()
+    np.random.seed(config['seed'])
+    torch.manual_seed(config['seed'])
+    torch.cuda.manual_seed_all(config['seed'])
+
     data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
     x,y = data_gen.get_data()
     # train test split
-    # x,y = np.random.rand(32,3,64,64,64),np.random.rand(32,1,64,64)
     xtr, xte, ytr,yte = train_test_split(x,y,test_size=0.2,random_state=42)
     xtr = torch.tensor(xtr,dtype=torch.float32)
     ytr = torch.tensor(ytr,dtype=torch.float32)
@@ -82,30 +80,42 @@ def main():
     train_dataset = TensorDataset(xtr, ytr)
     test_dataset = TensorDataset(xte, yte)
 
-    ## torch data loader ###
-    train_dataloader = DataLoader(train_dataset, batch_size= config['batch_size'], shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False)
+    sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False)
+    train_dataloader = DataLoader(train_dataset, config['batch_size'],sampler=sampler, pin_memory=True, num_workers=num_workers, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=config['batch_size'], num_workers=num_workers, shuffle=False)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    local_rank = rank - gpus_per_node * (rank // gpus_per_node)
+    torch.cuda.set_device(local_rank)
 
-    ### set a model ###
-    model = FinetuneNet()
-    model_dic = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/rotate/results/sql/test_model.pth'
+    model = FinetuneNet(config['model_grid'])
+    model_dic = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/best_model.pth'
     checkpoint = torch.load(model_dic,map_location=torch.device('cpu'))
     model.encoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('encoder')}
     model.decoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('decoder')}
-    model.to(device)
 
-    loss_object = SobelMse(device,0.8,0.2)
-    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], betas=(0.9, 0.999))
+    # model = Net(config['model_grid'])
+    model = model.to(local_rank)
+    ddp_model = DDP(model, device_ids=[local_rank],find_unused_parameters=True)
 
-
+    # Define the optimizer for the DDP model
+    optimizer = torch.optim.Adam(ddp_model.parameters(), lr=config['lr'], betas=(0.9, 0.999))
+    cheduler = StepLR(optimizer, step_size=50, gamma=0.2)
+    loss_object = SobelMse(local_rank, alpha=config['alpha'],beta=config['beta'])
+    # Create the Trainer instance
+    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size,scheduler=None)
+    
+    # Run the training and testing
     ### start training ###
+    trainer.eval()
+
     start = time.time()
-    # Assuming model, loss_object, optimizer, train_dataloader, test_dataloader, config, and device are defined
-    trainer = Trainer(model, loss_object, optimizer, train_dataloader, test_dataloader, config, device)
-    tr_losses, vl_losses = trainer.run()
+    print(f'rank {rank} starts training\n')
+    trainer.run()
     end = time.time()
     print(f'running time:{(end-start)/60} mins')
+    trainer.save(False)
+   
+    # Clean up
+    cleanup()
 if __name__ == '__main__':
     main()
