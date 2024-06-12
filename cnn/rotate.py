8fe10226973061427b3ef33c267e1ece0001f59a
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-12 20:28:57 +0100

    prepare fine tuning for rotate dataset with new configuration

--- cnn/rotate.py
+++ cnn/rotate.py
@@ -6,11 +6,11 @@ from torch.utils.data         import DataLoader, TensorDataset,DistributedSample
 from torch.nn.parallel        import DistributedDataParallel as DDP
 from torch.profiler           import profile, record_function, ProfilerActivity
 from torch.optim.lr_scheduler import StepLR
-from utils.preprocessing  import preProcessing,get_random_data
+from utils.preprocessing  import preProcessing
+from utils.ResNet3DModel  import FinetuneNet
+from utils.loss           import SobelMse,FreqMae,SobelMae,mean_absolute_percentage_error, calculate_ssim_batch
 from utils.trainclass     import ddpTrainer
-from utils.ResNet3DModel  import Net
-from utils.loss           import SobelMse,mean_absolute_percentage_error, calculate_ssim_batch
-from utils.config         import parse_args
+from utils.config         import parse_args,load_config,merge_config
 # from utils.plot           import img_plt
 
 import h5py as h5
@@ -25,9 +25,7 @@ import argparse
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 from sklearn.model_selection      import train_test_split 
-import socket 
-
-# torch.backends.cudnn.deterministic = True
+import socket                     
 def setup(rank, world_size):
     "Sets up the process group and configuration for PyTorch Distributed Data Parallelism"
 
@@ -55,26 +53,29 @@ def setup(rank, world_size):
 def cleanup():
     "Cleans up the distributed environment"
     dist.destroy_process_group()
-   
 def main(): 
     # Initialize any necessary components for DDP
     world_size    = int(os.environ.get("SLURM_NTASKS"))
     rank          = int(os.environ.get("SLURM_PROCID"))
     gpus_per_node = int(os.environ.get("SLURM_GPUS_ON_NODE"))
     num_workers = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
-    
     print(f"Hello from rank {rank} of {world_size} on {socket.gethostname()} where there are" \
           f" {gpus_per_node} allocated GPUs per node.", flush=True)
-    setup(rank, world_size) 
+    setup(rank, world_size)
 
-    config = parse_args()
-    np.random.seed(config['seed'])
-    torch.manual_seed(config['seed'])
-    torch.cuda.manual_seed_all(config['seed'])
+    # configuration of training
+    args = parse_args()
+    config = load_config(args.config)
+    config = merge_config(args, config)
 
-    x,y = get_random_data('/home/dc-su2/rds/rds-dirac-dr004/Magritte/random_grid64_data0.hdf5')
+    np.random.seed(config['model']['seed'])
+    torch.manual_seed(config['model']['seed'])
+    torch.cuda.manual_seed_all(config['model']['seed'])
+
+    data_gen = preProcessing(config['dataset']['path'])
+    x,y = data_gen.get_data()
     # train test split
-    # x,y = np.random.rand(64,3,64,64,64),np.random.rand(64,1,64,64)
+    x,y = np.random.rand(32,3,64,64,64),np.random.rand(32,1,64,64)
     xtr, xte, ytr,yte = train_test_split(x,y,test_size=0.2,random_state=42)
     xtr = torch.tensor(xtr,dtype=torch.float32)
     ytr = torch.tensor(ytr,dtype=torch.float32)
@@ -85,26 +86,28 @@ def main():
     test_dataset = TensorDataset(xte, yte)
 
     sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False)
-    train_dataloader = DataLoader(train_dataset, config['batch_size'],sampler=sampler, pin_memory=True, num_workers=num_workers, shuffle=False)
-    test_dataloader = DataLoader(test_dataset, batch_size=config['batch_size'], num_workers=num_workers, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, config['dataset']['batch_size'],sampler=sampler, pin_memory=True, num_workers=num_workers, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=config['dataset']['batch_size'], num_workers=num_workers, shuffle=False)
 
     local_rank = rank - gpus_per_node * (rank // gpus_per_node)
     torch.cuda.set_device(local_rank)
-    ### set a model ###
-    model = Net(64)
+
+    model = FinetuneNet(config['dataset']['grid'])
+    model_dic = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/best_model.pth'
+    checkpoint = torch.load(model_dic,map_location=torch.device('cpu'))
+    model.encoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('encoder')}
+    model.decoder_state_dict = {k: v for k, v in checkpoint.items() if k.startswith('decoder')}
+
+    # model = Net(config['model_grid'])
     model = model.to(local_rank)
-    model_dict = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/faceon.pth'
-    map_location = {'cuda:%d' % 0: 'cuda:%d' % local_rank}
-    model.load_state_dict(torch.load(model_dict, map_location=map_location),strict=False)
     ddp_model = DDP(model, device_ids=[local_rank],find_unused_parameters=True)
 
     # Define the optimizer for the DDP model
-    optimizer = torch.optim.Adam(ddp_model.parameters(), lr=config['lr'], betas=(0.9, 0.999))
-    # init larger step size and lr with setpLR
-    scheduler = StepLR(optimizer, step_size=200, gamma=0.1)
-    loss_object = SobelMse(local_rank, alpha=config['alpha'],beta=config['beta'])
+    optimizer = torch.optim.Adam(ddp_model.parameters(), lr=config['model']['lr'], betas=(0.9, 0.999))
+    cheduler = StepLR(optimizer, step_size=50, gamma=0.2)
+    loss_object = SobelMse(local_rank, alpha=config['model']['alpha'],beta=config['model']['beta'])
     # Create the Trainer instance
-    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size,scheduler=scheduler)
+    trainer = ddpTrainer(ddp_model, train_dataloader, test_dataloader, optimizer,loss_object,config,local_rank, world_size,scheduler=None)
     
     # Run the training and testing
     ### start training ###
