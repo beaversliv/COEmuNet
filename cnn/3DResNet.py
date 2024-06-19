f177177c5bc3bde79dd8bd2752d555c5a84e5431
beaversliv <shiqi.su@stfc.ac.uk>
2024-06-19 10:58:40 +0100

    update single GPU

--- cnn/3DResNet.py
+++ cnn/3DResNet.py
@@ -7,7 +7,8 @@ from utils.dataloader     import CustomTransform,IntensityDataset
 from utils.ResNet3DModel          import Net
 from utils.loss           import SobelMse
 from utils.plot           import img_plt,history_plt
-from utils.preprocessing  import preProcessing
+from utils.preprocessing  import preProcessing,get_data
+from utils.config         import parse_args,load_config,merge_config
 from utils.trainclass     import Trainer
 # helper packages
 import h5py as h5
@@ -34,59 +35,16 @@ if torch.cuda.is_available():
 else:
     print("No GPU available, using CPU.")
 
-
-# Global Constants
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-seed = 1234
-np.random.seed(seed)
-torch.manual_seed(seed) 
-torch.cuda.manual_seed(seed)
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path_dir', type = str, default = os.getcwd())
-    parser.add_argument('--model_name', type = str, default = 'trainlonger.pth')
-    parser.add_argument('--dataset', type = str, default = 'p3droslo')
-    parser.add_argument('--model_grid',type=int,default= 64,help='grid of hydro model:[32,64,128]')
-    parser.add_argument('--save_path',type =str, default = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/')
-    parser.add_argument('--logfile',type = str, default = 'log_file.txt')
-    parser.add_argument('--patience',type = int, default = 20, help='early stop patience')
-    parser.add_argument('--epochs', type = int, default = 1000)
-    parser.add_argument('--batch_size', type = int, default = 128)
-    parser.add_argument('--lr', type = float, default = 1e-3)
-    parser.add_argument('--lr_decay', type = float, default = 0.95)
-
-
-    args = parser.parse_args()
-    
-    config = OrderedDict([
-            ('path_dir', args.path_dir),
-            ('model_name', args.model_name),
-            ('dataset', args.dataset),
-            ('model_grid', args.model_grid),
-            ('save_path',args.save_path),
-            ('logfile',args.logfile),
-            ('patience',args.patience),
-            ('epochs', args.epochs),
-            ('batch_size', args.batch_size),
-            ('lr', args.lr),
-            ('lr_decay', args.lr_decay)
-            ])
-    
-    return config
-def postProcessing(y):
-    
-    min_ = -47.387955
-    median = 8.168968
-    y = y*median + min_
-    y = np.exp(y)
-    return y
-
 def main():
-    config = parse_args()
-    data_gen = preProcessing('/home/dc-su2/rds/rds-dirac-dr004/Magritte/faceon_grid64_data0.hdf5')
-    x,y = data_gen.get_data()
-    # x,y = np.random.rand(32,3,64,64,64),np.random.rand(32,1,64,64)
+    args = parse_args()
+    config = load_config(args.config)
+    config = merge_config(args, config)
+    
+    np.random.seed(config['model']['seed'])
+    torch.manual_seed(config['model']['seed'])
+    torch.cuda.manual_seed_all(config['model']['seed'])
+    # x,y = get_data(config['dataset']['path'])
+    x,y = np.random.rand(32,3,64,64,64),np.random.rand(32,1,64,64)
     
     # train test split
     xtr, xte, ytr,yte = train_test_split(x,y,test_size=0.2,random_state=42)
@@ -99,18 +57,19 @@ def main():
     test_dataset = TensorDataset(xte, yte)
 
     ## torch data loader ###
-    train_dataloader = DataLoader(train_dataset, batch_size= config['batch_size'], shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size= config['dataset']['batch_size'], shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=config['dataset']['batch_size'], shuffle=False)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     ### set a model ###
-    model = Net(config['model_grid']).to(device)
+    model = Net(config['dataset']['grid']).to(device)
+    model_dict = '/home/dc-su2/rds/rds-dirac-dp225-5J9PXvIKVV8/3DResNet/grid64/original/results/best/best_model.pth'
+    model.load_state_dict(torch.load(model_dict,map_location=device))
     
-    loss_object = SobelMse(device,alpha=0.8,beta=0.2)
-    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], betas=(0.9, 0.999))
-
-
+    loss_object = SobelMse(device,alpha=config['model']['alpha'],beta=config['model']['beta'])
+    optimizer_params = config['optimizer']['params']
+    optimizer = torch.optim.Adam(model.parameters(), **optimizer_params)
     ### start training ###
     start = time.time()
     # Assuming model, loss_object, optimizer, train_dataloader, test_dataloader, config, and device are defined
@@ -120,8 +79,6 @@ def main():
     print(f'running time:{(end-start)/60} mins')
     
     ### validation ###
-    trainer.save(model_path = config['save_path'] + 'model.pth', 
-                 history_path = config['save_path'] + 'history.pkl',
-                 history = history)
+    trainer.save(False)
 if __name__ == '__main__':
     main()
