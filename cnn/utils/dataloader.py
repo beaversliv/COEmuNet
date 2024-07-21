2e13803d325768dff297ea1b77b64c5618101e6a
beaversliv <shiqi.su@stfc.ac.uk>
2024-07-21 11:09:34 +0100

    ddp dataloader

--- cnn/utils/dataloader.py
+++ cnn/utils/dataloader.py
@@ -128,8 +128,38 @@ class IntensityDataset(Dataset):
             running_total += file_length
         raise IndexError("Index out of range")
     def remove_outlier(self,y):
-        y[y == 0] = np.min(y[y != 0])
+        y[y == 0.0] = np.min(y[y != 0.0])
         y = np.log(y)
         if np.min(y) <= -50:
             return True
         return False
+class MultiEpochsDataLoader(torch.utils.data.DataLoader):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._DataLoader__initialized = False
+        self.batch_sampler = _RepeatSampler(self.batch_sampler)
+        self._DataLoader__initialized = True
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield next(self.iterator)
+
+
+class _RepeatSampler(object):
+    """ Sampler that repeats forever.
+
+    Args:
+        sampler (Sampler)
+    """
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+    def __iter__(self):
+        while True:
+            yield from iter(self.sampler)
